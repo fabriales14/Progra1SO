52747724abe3a05f74366edee685e54ed6b9a087
Fabricio <fabriales14@gmail.com>
2018-10-19 14:18:10 -0600

    ultimos cambios

--- Proyecto1.c
+++ Proyecto1.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <fcntl.h>
+#include <string.h>
 
 void crearMrMeeseek();
 
@@ -11,8 +13,13 @@ int main() {
   srand(time(NULL)); // se necesita para generar randoms diferentes
   char tarea[100];
   int dificultad;
+  int contador = 0;
+  int instancia = 1;
   float tiempo;
   char c;
+  int p[2], readbytes;
+  char buffer[100];
+  pipe(p);
   printf("Ingrese la tarea a realizar\n");
   scanf("%[^\n]s", tarea);
   printf("La tarea es: %s\n", tarea);
@@ -23,11 +30,26 @@ int main() {
     scanf(" %d", &dificultad);
   } else {
     dificultad = rand() % 101;
-    tiempo = ((float)rand()/(float)(RAND_MAX)*5.0);
   }
+  tiempo = ((float)rand()/(float)(RAND_MAX)*5.0);
   printf("%d\n", dificultad);
   printf("%f\n", tiempo);
-  crearMrMeeseek();
+  if (dificultad > 85){
+    if (fork()){
+      contador++;
+      close(p[1]); // cerrar lado de escritura del pipe
+      readbytes = read(p[0], buffer, sizeof(buffer)); // lee la tarea
+      printf("%s\n", buffer);
+      close(p[0]);
+      printf("Hi I'm Mr Meeseeks! Look at Meeeee. (%d, %d, %d, %d)\n", getpid(), getppid(), contador, instancia);
+      printf("Resolving problem... (%d, %d, %d, %d)\n", getpid(), getppid(), contador, instancia);
+      sleep(tiempo);
+    } else {
+      close(p[0]); //cerrar lado de lectura del pipe
+      write(p[1], tarea, sizeof(tarea)); // escritura de la tarea
+      close(p[1]);
+    }
+  }
   wait(NULL);
   return 0;
 }
